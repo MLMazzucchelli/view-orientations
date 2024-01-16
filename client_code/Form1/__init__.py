dc725a53ddb7bac146a8ee728b002edb98da9ad2
mlmazzucc@gmail.com <mlmazzucc@gmail.com>
2024-01-16 18:38:12 +0000

    Created ServerModule1 and Form1.RowTemplate1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,26 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from plotly import graph_objects as go
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+    self.repeating_panel_orientations.items = anvil.server.call('get_orientations')
+
     # Any code you write here will run before the form opens.
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    new_plot = Plot(
+      data=go.Scattergl(
+        x=[0, 1, 2, 3, 4],
+        y=[0, 1, 4, 9, 16],
+      )
+    )
+    self.grid_panel_1.add_component(new_plot, width_xs=6)
